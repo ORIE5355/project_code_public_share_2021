2fc6dabe82ff60b604ce6458cc48c4505af93ec6
Nikhil Garg <nkgar6@gmail.com>
2021-11-24 21:29:42 -0800

    small edits

--- agents/yourteamname.py
+++ agents/yourteamname.py
@@ -46,7 +46,7 @@ class Agent(object):
     # Given an observation which is #info for new buyer, information for last iteration, and current profit from each time
     # Covariates of the current buyer
     # Data from last iteration (which item customer purchased, who purchased from, prices for each agent for each item (2x2, where rows are agents and columns are items)))
-    # Returns an action: a list of length n_items=2, indicating price this agent is posting for each item.
+    # Returns an action: a list of length n_items=2, indicating prices this agent is posting for each item.
     def action(self, obs):
         new_buyer_covariates, new_buyer_embedding, last_sale, profit_each_team = obs
         self._process_last_sale(last_sale, profit_each_team)
